e7ab5997c26426bda03a162af60afbbdeb4e3126
frnkwh <130784594+frnkwh@users.noreply.github.com>
2024-07-18 17:57:05 +0000

    testando script

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+
+# Install build-essential package
+sudo apt-get install build-essential
+
+# Install git
+sudo apt install git
+
+# Optionally, you might want to add a message indicating completion
+echo "Installation complete."
