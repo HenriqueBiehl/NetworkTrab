8d4d6dc272cd480f7c59c4aec3365a8f11109a91
Henrique <henriquebiehl@gmail.com>
2024-07-30 17:35:03 -0300

    New things, old mistakes

--- T2/testes/ring_token.c
+++ T2/testes/ring_token.c
@@ -8,66 +8,70 @@
 #define BUF_SIZE 256
 #define NUM_NODES 3
 
-void error_handling(const char *message) {
-    perror(message);
-    exit(EXIT_FAILURE);
+int bind_socket(int sock, struct sockaddr_in *addr, unsigned int index){
+    memset(addr, 0, sizeof(*addr));
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = htonl(INADDR_ANY);
+    addr->sin_port = htons(PORT_BASE + index);
+
+    if(bind(sock, (struct sockaddr*)addr, sizeof(*addr)) == -1)
+        return 0; 
+
+    return 1;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <node_index>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+void setar_proximo_nodo(struct sockaddr_in *node, unsigned int index){
+    memset(node, 0, sizeof(*node));
+    node->sin_family = AF_INET;
+    node->sin_addr.s_addr = htonl(INADDR_LOOPBACK);  // Para simplicidade, usando loopback
+    node->sin_port = htons(PORT_BASE + index);
+}
 
-    #Pega o indice do nodo na rede
-    int node_index = atoi(argv[1]);
-    int next_node_index = (node_index + 1) % NUM_NODES;
-
-    int sock;
-    struct sockaddr_in my_addr, next_node_addr, from_addr;
-    socklen_t addr_size;
+int main(int argc, char *argv[]){
+    int sock, index; 
+    struct sockaddr_in my_addr, next_node_addr, from_addr;   
     char buffer[BUF_SIZE];
-	
-	
+
+    index = atoi(argv[1]);
+    int next_node_index = (index + 1) % NUM_NODES;
+
     sock = socket(PF_INET, SOCK_DGRAM, 0);
-    if (sock == -1)
-        error_handling("socket() error");
+    if(sock == -1)
+        perror("Falha ao criar socket\n");
 
-    memset(&my_addr, 0, sizeof(my_addr));
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    my_addr.sin_port = htons(PORT_BASE + node_index);
+    if(!bind_socket(sock, &my_addr, index))
+        perror("Falha ao criar socket\n");
+    
+    printf("Socket para %d: %d\n", index, sock);
+    setar_proximo_nodo(&next_node_addr, next_node_index);
 
-    if (bind(sock, (struct sockaddr*)&my_addr, sizeof(my_addr)) == -1)
-        error_handling("bind() error");
-
-    memset(&next_node_addr, 0, sizeof(next_node_addr));
-    next_node_addr.sin_family = AF_INET;
-    next_node_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);  // Para simplicidade, usando loopback
-    next_node_addr.sin_port = htons(PORT_BASE + next_node_index);
-
-    if (node_index == 0) {
-        // Nó inicial começa com o bastão
-        snprintf(buffer, BUF_SIZE, "Token from node %d", node_index);
-        sendto(sock, buffer, strlen(buffer) + 1, 0, (struct sockaddr*)&next_node_addr, sizeof(next_node_addr));
-        printf("Node %d sent token to node %d\n", node_index, next_node_index);
+    if(index == 0){
+        snprintf(buffer, BUF_SIZE,"Bastão de %d\n", index);
+        int env = sendto(sock, buffer, strlen(buffer) + 1, 0, (struct sockaddr*)&next_node_addr, sizeof(next_node_addr));
+        if(env < 0) 
+            perror("Falha ao fazer sendto()\n");
+        
     }
 
-    while (1) {
+    socklen_t addr_size;
+    int opt; 
+    while(1){
+        printf("Waiting\n");
         addr_size = sizeof(from_addr);
+        printf("Sending buffer\n");
         int str_len = recvfrom(sock, buffer, BUF_SIZE, 0, (struct sockaddr*)&from_addr, &addr_size);
         if (str_len < 0)
-            error_handling("recvfrom() error");
+            printf("Nothing received in %d\n", index);
+        else 
+            printf("Received: %s\n", buffer);  
 
-        printf("Node %d received token: %s\n", node_index, buffer);
+        printf("Move on?\n");
+        scanf("%d", &opt);
+        if(opt)
+            break;
 
         sleep(1);  // Simulação de algum processamento
-
-        snprintf(buffer, BUF_SIZE, "Token from node %d", node_index);
-        sendto(sock, buffer, strlen(buffer) + 1, 0, (struct sockaddr*)&next_node_addr, sizeof(next_node_addr));
-        printf("Node %d sent token to node %d\n", node_index, next_node_index);
     }
 
     close(sock);
-    return 0;
 }
